2d494499b8e97fd21f6a3203b69946f32b72566f
Nicolas Dureisseix <nicolas.dureisseix@hotmail.fr>
2026-08-04 23:41:09 +0200

    UnixFileSystem begin impl

--- android-12.0.0/java/io/UnixFileSystem.cpp
+++ android-12.0.0/java/io/UnixFileSystem.cpp
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <dirent.h>
+
 #include <fmt/format.h>
 #include <jni/jni.h>
 
@@ -31,7 +33,44 @@
 
 /** @todo implementation **/
 
+using namespace sandvik;
+
 extern "C" {
+
+	JNIEXPORT jobjectArray JNICALL Java_java_io_UnixFileSystem_list0(JNIEnv* env, jobject thiz, jobject fileObj) {
+		auto jenv = sandvik::native::getNativeInterface(env);
+		auto& classloader = jenv->getClassLoader();
+
+		auto file = sandvik::native::getObject(fileObj);
+		auto pathObj = file->getField("path");
+		if (!pathObj->isString()) {
+			throw VmException("UnixFileSystem.list0: File.path is not a string");
+		}
+		std::string path = pathObj->str();
+
+		DIR* dir = opendir(path.c_str());
+		if (dir == nullptr) {
+			return nullptr;
+		}
+
+		std::vector<std::string> entries;
+		struct dirent* entry;
+		while ((entry = readdir(dir)) != nullptr) {
+			std::string name = entry->d_name;
+			if (name == "." || name == "..") {
+				continue;
+			}
+			entries.push_back(name);
+		}
+		closedir(dir);
+
+		auto& stringClass = classloader.getOrLoad("java/lang/String");
+		auto array = Array::make(stringClass, static_cast<uint32_t>(entries.size()));
+		for (size_t i = 0; i < entries.size(); ++i) {
+			array->setElement(static_cast<uint32_t>(i), Object::make(classloader, entries[i]));
+		}
+		return (jobjectArray)array;
+	}
 
 #if 0
 JNIEXPORT void JNICALL Java_java_io_UnixFileSystem_getBooleanAttributes0(JNIEnv* env, jobject obj) {
@@ -53,12 +92,6 @@ JNIEXPORT void JNICALL Java_java_io_UnixFileSystem_setPermission0(JNIEnv* env, j
 
 #if 0
 JNIEXPORT void JNICALL Java_java_io_UnixFileSystem_delete0(JNIEnv* env, jobject obj) {
-    logger.fwarning("{} not implemented!", __FUNCTION__);
-}
-#endif
-
-#if 0
-JNIEXPORT void JNICALL Java_java_io_UnixFileSystem_list0(JNIEnv* env, jobject obj) {
     logger.fwarning("{} not implemented!", __FUNCTION__);
 }
 #endif
